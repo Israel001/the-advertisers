93fd577faba5c9a0c805a4cb69d872f8f4d1dcb5
theCephas <oshoiseoluwa@gmail.com>
2024-07-22 14:24:42 +0100

    add to cart buttons

--- src/components/Helpers/InputQuantityCom.jsx
+++ src/components/Helpers/InputQuantityCom.jsx
@@ -1,32 +1,49 @@
 /* eslint-disable react/prop-types */
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useAppContext } from "../../contexts";
 
 export default function InputQuantityCom({ item }) {
   const [quantity, setQuantity] = useState(item?.quantity);
-  const { updateCartQty } = useAppContext();
+  const { updateCartQty, removeFromCart } = useAppContext();
+
+  useEffect(() => {
+    if (quantity === 0) {
+      removeFromCart(item);
+    }
+  }, [quantity, removeFromCart, item]);
+
+  const handleDecrement = () => {
+    if (quantity > 1) {
+      updateCartQty(item, quantity - 1);
+      setQuantity(quantity - 1);
+    } else if (quantity === 1) {
+      updateCartQty(item, 0);
+      setQuantity(0);
+    }
+  };
+
+  const handleIncrement = () => {
+    updateCartQty(item, quantity + 1);
+    setQuantity(quantity + 1);
+  };
+
+  if (quantity === 0) {
+    return null;
+  }
 
   return (
     <div className="w-[120px] h-[40px] px-[26px] flex items-center border border-qgray-border">
       <div className="flex justify-between items-center w-full">
         <button
-          onClick={() => {
-            if (quantity > 1) {
-              updateCartQty(item, quantity - 1);
-              setQuantity(quantity - 1);
-            }
-          }}
+          onClick={handleDecrement}
           type="button"
           className="text-base text-qgray"
         >
           -
         </button>
-        <span className="text-qblack">{item?.quantity}</span>
+        <span className="text-qblack">{quantity}</span>
         <button
-          onClick={() => {
-            updateCartQty(item, quantity + 1);
-            setQuantity(quantity + 1);
-          }}
+          onClick={handleIncrement}
           type="button"
           className="text-base text-qgray"
         >
